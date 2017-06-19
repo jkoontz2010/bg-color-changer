ed1a254ca2ba711c9cc2f99b08d3f8f56bf5ad0a
Jay Koontz <jkoontz2010@gmail.com>
2017-06-19 12:27:55 -0700

    wrapped in IIFE to keep namespace clean

--- color-changer.js
+++ color-changer.js
@@ -1,42 +1,46 @@
-function getRandomUnusedHue(usedHues) {
-    let randomHue;
-    while(randomHue === undefined || usedHues[randomHue]) {
-        randomHue = Math.floor(Math.random()*360);
-    }
-    return randomHue;
-}
+(function() {
 
-function changeBgHue(newHue) {
-    let minLightness = 30;
-    let maxLightness = 70;
-    const randomLightness = Math.floor(Math.random() * (maxLightness - minLightness) + minLightness)
-    document.body.style.backgroundColor = `hsl(${newHue}, 50%, ${randomLightness}%)`;
-}
-
-function changeBgColorRandomly() {
-    const usedHues = {};
-      
-    let randomBgColorChange = function() {
-        let newHue = getRandomUnusedHue(usedHues);
-        changeBgHue(newHue)
-        usedHues[newHue] = true;
+    function getRandomUnusedHue(usedHues) {
+        let randomHue;
+        while(randomHue === undefined || usedHues[randomHue]) {
+            randomHue = Math.floor(Math.random()*360);
+        }
+        return randomHue;
     }
 
-    let count=1;
-    randomBgColorChange();
-    count++;
+    function changeBgHue(newHue) {
+        let minLightness = 30;
+        let maxLightness = 70;
+        const randomLightness = Math.floor(Math.random() * (maxLightness - minLightness) + minLightness)
+        document.body.style.backgroundColor = `hsl(${newHue}, 50%, ${randomLightness}%)`;
+    }
 
-    let bgChangeInterval = setInterval(() => {
+    function changeBgColorRandomly() {
+        const usedHues = {};
+        
+        let randomBgColorChange = function() {
+            let newHue = getRandomUnusedHue(usedHues);
+            changeBgHue(newHue)
+            usedHues[newHue] = true;
+        }
+
+        let count=1;
         randomBgColorChange();
-        if(count === 10) clearInterval(bgChangeInterval);
         count++;
-    }, 1000);
-}
 
-function addSmoothTransitionToBody() {
-    document.body.style.transition = "background .5s";
-    document.body.style.transitionTimingFunction = "ease";
-}
+        let bgChangeInterval = setInterval(() => {
+            randomBgColorChange();
+            if(count === 10) clearInterval(bgChangeInterval);
+            count++;
+        }, 1000);
+    }
 
-addSmoothTransitionToBody();
-changeBgColorRandomly();
+    function addSmoothTransitionToBody() {
+        document.body.style.transition = "background .5s";
+        document.body.style.transitionTimingFunction = "ease";
+    }
+
+    addSmoothTransitionToBody();
+    changeBgColorRandomly();
+
+})();
